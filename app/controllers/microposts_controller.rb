518e15d59502cbf2fdeebb995ea7dafdfdc4ffd6
s-mori <pillows77@gmail.com>
2017-07-09 16:31:51 +0200

    change micropost find to find_by

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -64,7 +64,7 @@ class MicropostsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_micropost
-      @micropost = Micropost.find(params[:id])
+      @micropost = Micropost.find_by(user_id: params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
